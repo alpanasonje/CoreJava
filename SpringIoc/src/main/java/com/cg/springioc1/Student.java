874a5ca97be3fb807ee5e35404c56f43c4979986
Hp <Hp@DESKTOP-MEHK842.bbrouter>
2022-11-07 16:26:49 +0530

    commit

--- SpringIoc/src/main/java/com/cg/springioc1/Student.java
+++ SpringIoc/src/main/java/com/cg/springioc1/Student.java
@@ -6,29 +6,46 @@ public class Student {
 	private int rollNo;
 	private String name;
 	ArrayList<String> certificates;
-	
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(int rollNo, String name, ArrayList<String> certificates) {
+		super();
+		this.rollNo = rollNo;
+		this.name = name;
+		this.certificates = certificates;
+	}
+
 	public ArrayList<String> getCertificates() {
 		return certificates;
 	}
+
 	public void setCertificates(ArrayList<String> certificates) {
 		this.certificates = certificates;
 	}
+
 	public int getRollNo() {
 		return rollNo;
 	}
+
 	public void setRollNo(int rollNo) {
 		this.rollNo = rollNo;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	@Override
 	public String toString() {
 		return "Student [rollNo=" + rollNo + ", name=" + name + ", certificates=" + certificates + "]";
 	}
-	
-	
+
 }
--- SpringIoc/src/main/java/com/cg/springioc1/StudentDemo.java
+++ SpringIoc/src/main/java/com/cg/springioc1/StudentDemo.java
@@ -6,11 +6,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class StudentDemo {
 
 	public static void main(String[] args) {
+		///Student s1=new Student();
+		
 		ApplicationContext ac=new ClassPathXmlApplicationContext("beans.xml");
+		
 		Student s1=ac.getBean("st",Student.class);
 		System.out.println(s1);
 		
 		s1=ac.getBean("st1",Student.class);
+		System.out.println(s1);
+		
+		s1=ac.getBean("st2",Student.class);
 		System.out.println(s1);
 	}
 
