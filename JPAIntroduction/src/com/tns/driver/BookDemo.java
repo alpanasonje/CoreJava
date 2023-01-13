c3457df8e01e8efb52cd89a34530556aef2501d0
Hp <Hp@DESKTOP-MEHK842.bbrouter>
2023-01-13 17:38:46 +0530

    Commit

---
+++ JPAIntroduction/src/com/tns/driver/BookDemo.java
@@ -0,0 +1,40 @@
+package com.tns.driver;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.tns.jpaentity.Book;
+
+public class BookDemo {
+
+	public static void main(String[] args) {
+		try
+		{
+		
+		EntityManagerFactory factory =
+				Persistence.createEntityManagerFactory("PU");
+		
+				EntityManager em = factory.createEntityManager();
+				
+				em.getTransaction().begin();
+				
+				Book b1=new Book();
+				b1.setBookId(102);
+				b1.setTitle("Java Complete Reference");
+				b1.setAuthor("Herbert Schild");
+				b1.setPrice(2000);
+				
+				em.persist(b1);
+				em.getTransaction().commit();
+				System.out.println("Book record inserted....");
+				
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		System.out.println("--------------------------------------");
+	}
+
+}
