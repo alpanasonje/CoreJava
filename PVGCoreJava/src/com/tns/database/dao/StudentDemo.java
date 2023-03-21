81853444dddebf887b09aebf4aceff7e54fa9858
Hp <Hp@DESKTOP-MEHK842.mshome.net>
2023-03-21 10:54:03 +0530

    commit

---
+++ PVGCoreJava/src/com/tns/database/dao/StudentDemo.java
@@ -0,0 +1,34 @@
+package com.tns.database.dao;
+
+import java.sql.SQLException;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		//StudentServiceImpl service=new StudentServiceImpl();
+		StudentServiceImplUsingPreaparedStmt service=new StudentServiceImplUsingPreaparedStmt();
+		int n;
+		try {
+			n=service.addStudent(8, "Nikita", 67, 103);
+			if (n>0)
+				System.out.println("Record inserted successfully....");
+			
+			n=service.updateStudentPerByRollNo(101, 90);
+			if (n>0)
+				System.out.println("Record updated successfully....");
+			
+			
+			  n=service.deleteStudentByRollNo(3); if (n>0)
+			  System.out.println("Record deleted successfully....");
+			  
+			  n=service.deleteStudentByName("Nikhil"); if (n>0)
+			  System.out.println("Record deleted successfully....");
+			 
+			service.displayStudents();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
