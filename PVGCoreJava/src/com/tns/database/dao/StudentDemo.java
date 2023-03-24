48d74fcc057e43fec4c27b4518727061a67f6375
Hp <Hp@DESKTOP-MEHK842.mshome.net>
2023-03-24 10:27:44 +0530

    commit

--- PVGCoreJava/src/com/tns/database/dao/StudentDemo.java
+++ PVGCoreJava/src/com/tns/database/dao/StudentDemo.java
@@ -6,29 +6,33 @@ public class StudentDemo {
 
 	public static void main(String[] args) {
 		//StudentServiceImpl service=new StudentServiceImpl();
-		StudentServiceImplUsingPreaparedStmt service=new StudentServiceImplUsingPreaparedStmt();
-		int n;
+		/*
+		 * StudentServiceImplUsingPreaparedStmt service=new
+		 * StudentServiceImplUsingPreaparedStmt(); int n; try { n=service.addStudent(8,
+		 * "Nikita", 67, 103); if (n>0)
+		 * System.out.println("Record inserted successfully....");
+		 * 
+		 * n=service.updateStudentPerByRollNo(101, 90); if (n>0)
+		 * System.out.println("Record updated successfully....");
+		 * 
+		 * 
+		 * n=service.deleteStudentByRollNo(3); if (n>0)
+		 * System.out.println("Record deleted successfully....");
+		 * 
+		 * n=service.deleteStudentByName("Nikhil"); if (n>0)
+		 * System.out.println("Record deleted successfully....");
+		 * 
+		 * service.displayStudents(); } catch (SQLException e) {
+		 * System.out.println(e.getMessage()); }
+		 */
+		UsingCallableStatement service =new UsingCallableStatement();
 		try {
-			n=service.addStudent(8, "Nikita", 67, 103);
-			if (n>0)
-				System.out.println("Record inserted successfully....");
+			//service.addStudent(9, "Sakshi", 77, 101);
+			System.out.println(service.getName(8));
 			
-			n=service.updateStudentPerByRollNo(101, 90);
-			if (n>0)
-				System.out.println("Record updated successfully....");
-			
-			
-			  n=service.deleteStudentByRollNo(3); if (n>0)
-			  System.out.println("Record deleted successfully....");
-			  
-			  n=service.deleteStudentByName("Nikhil"); if (n>0)
-			  System.out.println("Record deleted successfully....");
-			 
-			service.displayStudents();
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
-
 	}
 
 }
